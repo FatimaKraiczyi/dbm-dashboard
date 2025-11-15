b0196f022716709fcfc380ead2d1130a8e1ce941
Fátima Kraiczyi <fatimakraiczyi@gmail.com>
2025-11-14 23:25:08 -0300

    Refatora estrutura de estilos: remove arquivos CSS antigos, adiciona novo tema e estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './routes/AppRoutes'
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import theme from './assets/styles/theme'
 
 function App() {
 
   return (
-    <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <CssBaseline />
         <AppRoutes />
-    </BrowserRouter>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
